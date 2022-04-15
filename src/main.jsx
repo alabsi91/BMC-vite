e69b024fe33d1a6ef44f65dc558a31063e38980c
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2022-04-15 22:37:11 +0300

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register';
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
