0c17f9a5a2c8fad48b7209863a5addfd005c3c8c
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2022-04-15 22:24:04 +0300

    23424

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
